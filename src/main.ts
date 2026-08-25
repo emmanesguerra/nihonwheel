70ba2e8158c78a0718219f567cdf6ba5e6c98957
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2026-08-25 20:39:28 +0800

    initial upload

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'  
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './style.css'
+import './assets/styles/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+createApp(App)
+  .use(router)
+  .mount('#app')
